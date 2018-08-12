f0085dafec809aba6fa9652f84419f4a607162ee
Godfrey <godfreyjr@gmail.com>
2018-08-12 00:11:25 -0500

    style: create different image sizes and update index page to get appropriate images

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+/*
+ After you have changed the settings under responsive_images
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'gm',
+          sizes: [{
+            width: 270,
+            quality: 50,
+            suffix: '_1x'
+          },
+          {
+            width: 540,
+            quality: 50,
+            suffix: '_2x'
+          },
+          {
+            width: 1600,
+            suffix: '_2x',
+            upscale: true
+          }]
+        },
+
+        /*
+        You don't need to change this part if you don't change
+        the directory structure.
+        */
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'img/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['images'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['images']
+        },
+      },
+    },
+
+    /* Copy the images that don't go through processing into the images/directory */
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: ['img/*.{gif,jpg,png}'],
+          dest: 'images/',
+          flatten: true,
+        }]
+      },
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+
+};
--- js/main.js
+++ js/main.js
@@ -161,6 +161,8 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  const prefix = '/images/'+restaurant.id;
+  image.srcset = prefix+'-270_1x.jpg 1x, '+prefix+'-540_2x.jpg 2x';
   li.append(image);
 
   const name = document.createElement('h1');
